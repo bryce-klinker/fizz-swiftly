8f9d30f72fd054a19dafaa21056ea55c14d682c2
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-20 14:49:21 -0600

    Refactored fizzBuzz method to be easier to read

--- Fizzly/Fizzly/FizzBuzz.swift
+++ Fizzly/Fizzly/FizzBuzz.swift
@@ -1,17 +1,26 @@
 import Foundation
 
 func fizzBuzz(value: CurrentValue) -> String {
-    if (value.value % 3 == 0 && value.value % 5 == 0) {
+    if (isDivisibleByThree(value: value)
+        && isDivisibleByFive(value: value)) {
         return "FizzBuzz"
     }
     
-    if (value.value % 3 == 0) {
+    if (isDivisibleByThree(value: value)) {
         return "Fizz"
     }
     
-    if (value.value % 5 == 0) {
+    if (isDivisibleByFive(value: value)) {
         return "Buzz"
     }
     
     return String(value.value)
+}
+
+private func isDivisibleByThree(value: CurrentValue) -> Bool {
+    return value.value % 3 == 0
+}
+
+private func isDivisibleByFive(value: CurrentValue) -> Bool {
+    return value.value % 5 == 0
 }
