d29c9c7472dfc64719f955a7a816079606a2cf1f
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-20 15:06:10 -0600

    Removing pods from repo

--- Fizzly/Pods/Target Support Files/Pods-FizzlyTests/Pods-FizzlyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FizzlyTests : NSObject
-@end
-@implementation PodsDummy_Pods_FizzlyTests
-@end
--- Fizzly/Pods/Target Support Files/Pods-FizzlyUITests/Pods-FizzlyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FizzlyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FizzlyUITests
-@end
